a18ccd61dd0c8f3833f168bc07d24a8144f423ba
VincentiusF <vincentiusfernando_2226240032@mhs.mdp.ac.id>
2024-04-18 14:02:16 +0700

    soal1.html

---
+++ Soal 1/script.js
@@ -0,0 +1,23 @@
+// lanjutkan script ini ya ...
+let nama = "Arif";
+localStorage.setItem("nama", nama);
+let hobi = ["Baca", "Bermain Game", "Belajar Coding"];
+localStorage.setItem("hobi", hobi);
+
+let mhs = {nama : "Arif", hobi : "Belajar Coding"};
+console.log(mhs.nama);
+console.log(mhs.hobi);
+
+
+
+let listhobi =[];
+function getlisthobi() {
+    listhobi.innerHTML = "";
+     if(localStorage.getItem("hobi")) {
+        data1 = JSON.parse(localStorage.getItem("testimoni"));
+        for(const [index, row] of listhobi.entries()) {
+             list.innerHTML += `<li>${row.nama} ${row.hobi}</li>`;
+}
+     }
+}
+getlisthobi();
